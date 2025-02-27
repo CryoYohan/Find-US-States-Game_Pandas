ccf1944ec97d456f383efe99c6bbbefeeaebbd6c
itsmecyril <cyrilypil@gmail.com>
2025-02-27 23:28:44 +0800

    added docu, error trapping

--- main.py
+++ main.py
@@ -42,28 +42,38 @@ class Main:
         while game_is_running:
 
             # Prompt user state to guess
+            try:
+                state = self.prompt.textinput(title=f'{self.states_guessed}/50 guessed!',
+                                              prompt="What's the next state's name?").title()
 
-            state = self.prompt.textinput(title=f'{self.states_guessed}/50 guessed!', prompt="What's the next state's name?")
 
-            if not state is None: # If none, then User clicked Cancel button. If so, the game closes
-                if state.title() in self.states_list and state.title() not in self.correct_guesses:
-                    print('Correct!')
-                    self.states_guessed += 1
-                    xcor = self.df[self.df.state == state.title()].x.to_list()[0] # Get State X coordinates from DataFrame
-                    ycor = self.df[self.df.state == state.title()].y.to_list()[0] # Get State Y coordinates from DataFrame
-                    print(f'X coord: {xcor} Y coord: {ycor}')
+                if not state is None: # If none, then User clicked Cancel button. If so, the game closes
+                    if state in self.states_list and state not in self.correct_guesses:
+                        print('Correct!')
+                        self.states_guessed += 1
+                        xcor = self.df[self.df.state == state].x.to_list()[0] # Get State X coordinates from DataFrame
+                        ycor = self.df[self.df.state == state].y.to_list()[0] # Get State Y coordinates from DataFrame
+                        print(f'X coord: {xcor} Y coord: {ycor}')
 
-                    self.correct_guesses.append(state.title()) # Add correct guesses to validate whether the input has already been guessed
+                        self.correct_guesses.append(state) # Add correct guesses to validate whether the input has already been guessed
 
-                    state = States(state=state,x=xcor, y=ycor) # Initialize State-turtle object
-                    state.move() # Move the State-turtle object to the given coordinates during initialization
+                        state = States(state=state,x=xcor, y=ycor) # Initialize State-turtle object
+                        state.move() # Move the State-turtle object to the given coordinates during initialization
 
-                elif state.title() in self.correct_guesses:
-                    print('You already guessed this state.')
+                    elif state in self.correct_guesses:
+                        # If state already guessed
+                        print('You already guessed this state.')
+                    else:
+                        # Input is not a state in US
+                        print('Meh')
                 else:
-                    print('Meh')
+                    # Close game if cancelled
+                    print('Thank you for playing!')
+                    game_is_running = False
 
-            else:
+            except AttributeError:
+                # When cancelled it'll return an AttributeError due to the prompt=, it's because if it's None, None has no .title() function
+                print('Thank you for playing!')
                 game_is_running = False
 
 
